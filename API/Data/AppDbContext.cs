7ff1376aa6b6a0763142a7d9552c765de64771fc
joaoggomes98 <joao.g.gomes1998@hotmail.com>
2025-10-23 01:58:33 +0100

    Primeiro commit - API criada

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+using System;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set;}
+}
---
+++ API/Models/AppUser.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace API.Models;
+
+ public class AppUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
